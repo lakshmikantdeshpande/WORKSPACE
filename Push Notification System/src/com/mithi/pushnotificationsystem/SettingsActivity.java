3716bd4bcf8f187555e4b641fc83d2f9ed574d49
Lakshmikant A. Deshpande <lakshmikantdeshpande@gmail.com>
2015-01-11 12:29:01 +0530

    Added Navigation to Settings Activity
    To comply with Android's navigation design guidelines...

--- Push Notification System/src/com/mithi/pushnotificationsystem/SettingsActivity.java
+++ Push Notification System/src/com/mithi/pushnotificationsystem/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.mithi.pushnotificationsystem;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 public class SettingsActivity extends PreferenceActivity {
 	EditTextPreference editTextPreference;
@@ -12,7 +14,25 @@ public class SettingsActivity extends PreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		
+		//Back button 
+	    getActionBar().setDisplayHomeAsUpEnabled(true);
 
+	}
+	
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	   
+	    // Respond to the action bar's Up/Home button
+	    case android.R.id.home:
+	        NavUtils.navigateUpFromSameTask(this);
+	        return true;
+	        
+	    }
+	    return super.onOptionsItemSelected(item);
 	}
 
 }
