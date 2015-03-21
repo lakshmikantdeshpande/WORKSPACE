7c63087fae437fcd91221486317a1b20c6504406
Lakshmikant A. Deshpande <lakshmikantdeshpande@gmail.com>
2015-03-22 05:01:29 +0530

    MILESTONE Nav Drawer works 5:00 AM 22/03/15
    Navigation drawer works smoothly and allows to switch fragments without
    any problems...
    Code not cleaned up yet !!

---
+++ Mithi Android Notifier/src/com/mithi/androidnotifier/SecondFragment.java
@@ -0,0 +1,38 @@
+package com.mithi.androidnotifier;
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class SecondFragment extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+ 
+    public static SecondFragment newInstance(int sectionNumber) {
+        SecondFragment fragment = new SecondFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    public SecondFragment() {
+ 
+    }
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_second, container,
+                false);
+ 
+        return rootView;
+    }
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(getArguments().getInt(
+                ARG_SECTION_NUMBER));
+    }
+}
