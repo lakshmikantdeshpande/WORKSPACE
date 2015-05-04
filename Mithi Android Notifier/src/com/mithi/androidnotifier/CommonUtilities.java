34a9288e7447c93c74d11390ceeb41c0a68363d4
Lakshmikant A. Deshpande <lakshmikantdeshpande@gmail.com>
2015-05-04 16:56:03 +0530

    Command Line push notification

--- Mithi Android Notifier/src/com/mithi/androidnotifier/CommonUtilities.java
+++ Mithi Android Notifier/src/com/mithi/androidnotifier/CommonUtilities.java
@@ -8,9 +8,15 @@ public final class CommonUtilities {
 	//This should be a hard-coded URL of MITHI servers
    // Registration URL
    //static final String SERVER_URL = "http://192.168.188.1/gcmdemo/register.php";
+ // static final String SERVER_URL = "http://192.168.43.194/gcmdemo/register.php";
+ //  static final String LOGOUT_URL = "http://192.168.43.194/gcmdemo/logout.php";
+
+   
    static final String SERVER_URL = "http://192.168.43.194/gcmdemo/register.php";
    static final String LOGOUT_URL = "http://192.168.43.194/gcmdemo/logout.php";
 
+   
+   
    // Google project id ... to be created by Mithi
    static final String SENDER_ID = "69552156334"; 
 
